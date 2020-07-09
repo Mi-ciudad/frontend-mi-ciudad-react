3445561db44927553a9bcd92ffc89cd45e8e7be1
Belen <angela.campos@anima.edu.uy>
2020-07-09 20:08:18 -0300

    Help page

---
+++ src/components/HelpData.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+class HelpData extends React.Component{
+
+    render(){
+        return(
+            <section>
+                <div className = "Help-card">
+                    <p>Esta es la caja de texto que se usa para que soluciones tu problema para que no tengas que arreglarlo solito el problema, para eso estamos, saludo de parte del equipo de Mi ciudad y esperamos que puedas solucionar este problema, un beso</p>
+                </div>
+                <div className = "Help-card">
+                    <p>Esta es la caja de texto que se usa para que soluciones tu problema para que no tengas que arreglarlo solito el problema, para eso estamos, saludo de parte del equipo de Mi ciudad y esperamos que puedas solucionar este problema, un beso</p>
+                </div>
+                <div className = "Help-card">
+                    <p>Esta es la caja de texto que se usa para que soluciones tu problema para que no tengas que arreglarlo solito el problema, para eso estamos, saludo de parte del equipo de Mi ciudad y esperamos que puedas solucionar este problema, un beso</p>
+                </div>
+                <div className = "Help-card">
+                    <p>Esta es la caja de texto que se usa para que soluciones tu problema para que no tengas que arreglarlo solito el problema, para eso estamos, saludo de parte del equipo de Mi ciudad y esperamos que puedas solucionar este problema, un beso</p>
+                </div>
+            </section>
+            )
+        }
+    }
+export default HelpData;
---
+++ src/pages/Help.js
@@ -0,0 +1,12 @@
+import React from "react";
+import HelpData from "../components/HelpData";
+
+class Help extends React.Component {
+    //el padre tiene el array y los metodos y se los da a los hijos
+    render() {
+        return (
+            <HelpData></HelpData>
+        )
+    }
+}
+export default Help;
