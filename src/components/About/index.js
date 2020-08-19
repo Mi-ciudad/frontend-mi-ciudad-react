41f904ec21b8eaee0113347d8689ec18e7b22f5b
Tiziana <tiziana@arcadiansoftare.co>
2020-08-19 16:57:11 -0300

    about page

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import './style.scss';
 
+import IconCity from '../../assets/images/city.png';
+
+
 const AboutComponent = () =>  {
         
         return (    
-            <div className>HOLA</div>                
+            <section className="about-us-section">
+                <div className="team-presentation">
+                    <figure>
+                        <img src={IconCity} alt='city' className="city" /> 
+                    </figure>
+                </div>
+            </section>                
         )
 
 
