61e562876f0c7fdc679e62e4a53e3b8e11ffcb42
Tiziana <tiziana@arcadiansoftare.co>
2020-09-25 08:47:31 -0300

    remove hooks

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,41 +1,41 @@
-import { useState, useEffect} from "react";
+// import { useState, useEffect} from "react";
 
-export const useFetch = (endpoint) => {
-    const [data, setData] = useState({});
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = usestate(null);
-    const [headersData, setHeadersData] = useState({});
+// export const useFetch = (endpoint) => {
+//     const [data, setData] = useState({});
+//     const [loading, setLoading] = useState(true);
+//     const [error, setError] = usestate(null);
+//     const [headersData, setHeadersData] = useState({});
 
-    useEffect(() => {
-        const fetchResource = async () => {
-            try{
-                const res = await fetch(endpoint);
+//     useEffect(() => {
+//         const fetchResource = async () => {
+//             try{
+//                 const res = await fetch(endpoint);
 
-                if(res.status < 200 || res.status >= 400 ) throw Erorr(res.body.errors);
+//                 if(res.status < 200 || res.status >= 400 ) throw Erorr(res.body.errors);
 
-                const getHeadersData = () => {
-                    const headers = {};
+//                 const getHeadersData = () => {
+//                     const headers = {};
 
-                    res.headers.forEach((value, name) => {
-                        headers[name] = value;
-                    })
+//                     res.headers.forEach((value, name) => {
+//                         headers[name] = value;
+//                     })
 
-                    return headers;
-                }
+//                     return headers;
+//                 }
 
-                setHeadersData(getHeadersData);
+//                 setHeadersData(getHeadersData);
                 
-                const resData = await res.json();
-                setData(resData);
-            } catch (e) {
-                setError(e);
-            } finally{
-                setLoading(false);
-            }
-        }
+//                 const resData = await res.json();
+//                 setData(resData);
+//             } catch (e) {
+//                 setError(e);
+//             } finally{
+//                 setLoading(false);
+//             }
+//         }
 
-        fetchResource();
-    }, [endpoint]);
+//         fetchResource();
+//     }, [endpoint]);
 
-    return { data, loading, error, headersdata};
-}
+//     return { data, loading, error, headersdata};
+// }
